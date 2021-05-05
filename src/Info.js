6cbe527c576d08f2df85543f03e4a29fe0f07fb0
Shehzad Daredia <shehzad.daredia@gmail.com>
2021-05-04 19:56:45 -0700

    Wire up UI to account data fetched from server. Handle happy path where one account is returned from server

--- src/Info.js
+++ src/Info.js
@@ -52,43 +52,51 @@ const Info = () => {
   if (error)
     return <div className="notice error">{error}</div>;
 
-  {/*return <pre>{JSON.stringify(accounts, undefined, 2)}</pre>;*/}
-
   return (
     <div>
       <div className="info-card">
-        <div className="info-card-contact">XYZ-101</div>
-        <div className="info-card-link"><FrontLink href='http://collectronics.com' label='View in Collectronics' /></div>
+        <div className="info-card-contact">{account.name}</div>
+        <div className="info-card-link"><FrontLink href={account.url} label='View in Collectronics' /></div>
       </div>
       <div className="info-card">
         <div className="data-title">Association</div>
-        <div>Florida Association</div>
+        <div>{account.associationName}</div>
       </div>
       <div className="info-card">
         <div className="data-title">Management Company</div>
-        <div>Demo Company</div>
+        <div>{account.companyName}</div>
       </div>
       <div className="info-card">
         <div className="data-title">Workflow Stage</div>
-        <div>Payment Plan</div>
+        <div>{account.workflowStage}</div>
       </div>
-      <div className="info-contact">
-        { accounts.length ? (
-          <>
-            <div className="data-title">Account</div>
-            <ul className="list-data">
-              {accounts.map((e, idx) => <li key={idx}>
-                <div className="info-entry">
-                  <div className="info-label">Name</div>
-                  <div className="info-value">Christian East</div>
-                </div>
-              </li>)}
-            </ul>
-          </>
-        ) : undefined }
-      </div>
+      {account.contactInfos.map((contact, idx) => (
+        <div className="info-contact" key={idx}>
+          <div className="data-title">Account</div>
+          <ul className="list-data">
+            <li>
+              <div className="info-entry">
+                <div className="info-label">Name</div>
+                <div className="info-value">{contact.name}</div>
+              </div>
+            </li>
+            <li>
+              <div className="info-entry">
+                <div className="info-label">Phone</div>
+                <div className="info-value">{contact.phone}</div>
+              </div>
+            </li>
+            <li>
+              <div className="info-entry">
+                <div className="info-label">Email</div>
+                <div className="info-value">{contact.email}</div>
+              </div>
+            </li>
+          </ul>
+        </div>)
+      )}
     </div>
-  )
+  );
 };
 
 export default Info;
